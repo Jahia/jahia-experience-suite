71903836ca724f633f1f509e1bb52c2c50e4995e
Cedric Mailleux <cmailleux@jahia.com>
2025-05-12 15:16:34 -0400

    fix: token creation for just 3 months

--- provisioning/create-token.groovy
+++ provisioning/create-token.groovy
@@ -1,11 +1,21 @@
-	
+import java.time.LocalDate
+import java.util.Calendar
+
 setResult("remove");
 org.jahia.services.content.JCRTemplate.getInstance().doExecuteWithSystemSession({ session ->
+    LocalDate months = LocalDate.now().plusMonths(3)
     org.jahia.osgi.BundleUtils.getOsgiService("org.jahia.modules.apitokens.TokenService")
         .tokenBuilder("/users/root", "test-token12345", session)
         .setToken("kgHNm05iQV61I+GY3X5HVr13i866HAAsyou8G+eGubk=")
         .setActive(true)
-        .setExpirationDate(new org.joda.time.DateTime('2025-12-31').toCalendar(Locale.getDefault()))
+        .setExpirationDate(toCalendar(months))
         .create()
     session.save();
 })
+
+Calendar toCalendar(LocalDate localDate) {
+    Calendar calendar = Calendar.getInstance();
+    calendar.clear();
+    calendar.set(localDate.getYear(), localDate.getMonthValue() - 1, localDate.getDayOfMonth());
+    return calendar;
+}
